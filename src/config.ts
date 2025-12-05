83f2e4e3bceed87f144d29be69ad07f85e7be335
axingsvg <3891035609@qq.com>
2025-12-06 05:42:48 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -135,7 +135,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "美しいミズキ", // 主页横幅主标题
+			title: "春知晓梦不觉恰似你我那年，不经事却说离别。", // 主页横幅主标题
 
 			subtitle: [
 				"特別なことはないけど、君がいると十分です",
